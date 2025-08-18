5aaf2e5a9e8054ba3a55983973e5393b322824ce
JOrihAlice <orihalice52@gmail.com>
2025-08-18 19:34:58 -0300

    Basico de camera e movimento para o player

--- Characters/Player/pc.gd
+++ Characters/Player/pc.gd
@@ -1,1 +1,14 @@
 extends CharacterBody2D
+
+@export var pc_speed = 200
+
+
+func get_input():
+	var direcao = Input.get_vector("Esquerda", "Direita", "Cima", "Baixo")
+	velocity = direcao * pc_speed
+	pass
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	pass
